e945f23507e6215f9cedac3049727fb6dce13696
Nguyễn Tử Trung <100124987+NguyenTuTrung@users.noreply.github.com>
2024-03-29 15:06:35 +0700

    Update BlackBox.java

--- src/src/BlackBox.java
+++ src/src/BlackBox.java
@@ -1,41 +1,102 @@
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class BlackBox {
+
+    // Junior
     @Test
-    void testcase1() {
-        Person p = new Person(-5, "NORMAL");
-        assertEquals(-1, p.account(p));
+    void testCase1() {
+        Person p = new Person(0, "Junior");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
+    }
+    @Test
+    void testCase2() {
+        Person p = new Person(1, "Junior");
+        assertEquals("Khong thue", p.isRent(p));
+    }
+    @Test
+    void testCase3() {
+        Person p = new Person(20, "Junior");
+        assertEquals("Thue ban thoi gian", p.isRent(p));
+    }
+    @Test
+    void testCase4() {
+        Person p = new Person(30, "Junior");
+        assertEquals("Thue toan thoi gian", p.isRent(p));
+    }
+    @Test
+    void testCase5() {
+        Person p = new Person(2147483646, "Junior");
+        assertEquals("Khong thue", p.isRent(p));
+    }
+    @Test
+    void testCase6() {
+        Person p = new Person(2147483647, "Junior");
+        assertEquals("Khong thue", p.isRent(p));
     }
 
+    // Senior
     @Test
-    void testcase2() {
-        Person p = new Person(4, "NORMAL");
-        assertEquals(100000, p.account(p));
+    void testCase7() {
+        Person p = new Person(0, "Senior");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
+    }
+    @Test
+    void testCase8() {
+        Person p = new Person(1, "Senior");
+        assertEquals("Khong thue", p.isRent(p));
+    }
+    @Test
+    void testCase9() {
+        Person p = new Person(20, "Senior");
+        assertEquals("Thue toan thoi gian", p.isRent(p));
+    }
+    @Test
+    void testCase10() {
+        Person p = new Person(30, "Senior");
+        assertEquals("Thue toan thoi gian", p.isRent(p));
+    }
+    @Test
+    void testCase11() {
+        Person p = new Person(2147483646, "Senior");
+        assertEquals("Khong thue", p.isRent(p));
+    }
+    @Test
+    void testCase12() {
+        Person p = new Person(2147483647, "Senior");
+        assertEquals("Khong thue", p.isRent(p));
     }
 
+    // Chuoi khac
     @Test
-    void testcase3() {
-        Person p = new Person(20, "NORMAL");
-        assertEquals(500000, p.account(p));
+    void testCase13() {
+        Person p = new Person(0, "abc");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
     }
-
     @Test
-    void testcase4() {
-        Person p = new Person(-4, "VIP");
-        assertEquals(-1, p.account(p));
+    void testCase14() {
+        Person p = new Person(1, "abc");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
     }
-
     @Test
-    void testcase5() {
-        Person p = new Person(3, "VIP");
-        assertEquals(200000, p.account(p));
+    void testCase15() {
+        Person p = new Person(20, "abc");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
     }
-
     @Test
-    void testcase6() {
-        Person p = new Person(25, "VIP");
-        assertEquals(1000000, p.account(p));
+    void testCase16() {
+        Person p = new Person(30, "abc");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
+    }
+    @Test
+    void testCase17() {
+        Person p = new Person(2147483646, "abc");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
+    }
+    @Test
+    void testCase18() {
+        Person p = new Person(2147483647, "abc");
+        assertEquals("Dau vao khong hop le", p.isRent(p));
     }
 }
